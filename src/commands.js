a33b1151d772ca493fbd4e1c2ca4f92e385155b9
Sean Lasater <SeanLasater@gmail.com>
2026-02-19 17:48:56 -0800

    add original files from sample app

---
+++ src/commands.js
@@ -0,0 +1,9 @@
+export const TUNEDOWNFORCE_COMMAND = {
+  name: 'Tune-Downforce',
+  description: 'Tune for optimal downforce based on weight, balance, and tire.',
+};
+
+export const TUNETRANSMISSION_COMMAND = {
+  name: 'Tune-Transmission',
+  description: 'Tune transmission based on track and car.',
+};
